8cbbe6bd0171b60980fd6da5daefa55748835ebd
benjamin.slabbert <benjamin.slabbert@gmail.com>
2024-03-13 21:12:26 +0100

    update build

--- plugin-parent/plugin-consumer/src/main/java/com/example/plugin/consumer/Main.java
+++ plugin-parent/plugin-consumer/src/main/java/com/example/plugin/consumer/Main.java
@@ -1,12 +1,9 @@
 /* Licensed under Apache-2.0 2024. */
 package com.example.plugin.consumer;
 
-import com.example.plugin.consumer.gen.api.model.Post;
-
 public class Main {
 
   public static void main(String[] args) {
-    Post post = Post.builder().id(1).title("title").body("body").registered(true).build();
-    System.err.println("post: " + post);
+    // do nothing here
   }
 }
