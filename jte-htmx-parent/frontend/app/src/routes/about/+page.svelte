7f739aa6d728d1965cd883f474f133c5f1140958
benjamin.slabbert <benjamin.slabbert@gmail.com>
2024-03-19 19:47:02 +0100

    fmt

--- jte-htmx-parent/frontend/app/src/routes/about/+page.svelte
+++ jte-htmx-parent/frontend/app/src/routes/about/+page.svelte
@@ -1,17 +1,17 @@
 <script>
-  const host = import.meta.env.VITE_JTE_HOST;
+	const host = import.meta.env.VITE_JTE_HOST;
 </script>
 
 <div id="response-div" hx-ext="response-targets">
-  <button
-    hx-post="{host}/clicked?q=345"
-    hx-swap="outerHTML"
-    hx-target="#response-div"
-    hx-target-404="#not-found"
-    hx-target-500="#serious-errors"
-  >
-    Register!
-  </button>
-  <div id="serious-errors"/>
-  <div id="not-found"/>
+	<button
+		hx-post="{host}/clicked?q=345"
+		hx-swap="outerHTML"
+		hx-target="#response-div"
+		hx-target-404="#not-found"
+		hx-target-500="#serious-errors"
+	>
+		Register!
+	</button>
+	<div id="serious-errors" />
+	<div id="not-found" />
 </div>
