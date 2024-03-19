e57f2d57cabea58f8799c61865c99ebc3362d0cc
benjamin.slabbert <benjamin.slabbert@gmail.com>
2024-03-19 19:40:48 +0100

    adding htmx extensions

--- jte-htmx-parent/frontend/app/src/routes/about/+page.svelte
+++ jte-htmx-parent/frontend/app/src/routes/about/+page.svelte
@@ -1,5 +1,17 @@
 <script>
-	const host = import.meta.env.VITE_JTE_HOST;
+  const host = import.meta.env.VITE_JTE_HOST;
 </script>
 
-<button hx-post="{host}/clicked?q=345" hx-swap="outerHTML">Click Me</button>
+<div id="response-div" hx-ext="response-targets">
+  <button
+    hx-post="{host}/clicked?q=345"
+    hx-swap="outerHTML"
+    hx-target="#response-div"
+    hx-target-404="#not-found"
+    hx-target-500="#serious-errors"
+  >
+    Register!
+  </button>
+  <div id="serious-errors"/>
+  <div id="not-found"/>
+</div>
