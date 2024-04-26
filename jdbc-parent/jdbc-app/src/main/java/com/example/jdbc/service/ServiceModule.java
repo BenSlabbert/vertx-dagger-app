0285d94ba07ed90f864dcfb5b8dc83283299848f
benjamin.slabbert <benjamin.slabbert@gmail.com>
2024-04-26 20:38:59 +0200

    add jdbc module
    upgrade docker runtimes

---
+++ jdbc-parent/jdbc-app/src/main/java/com/example/jdbc/service/ServiceModule.java
@@ -0,0 +1,10 @@
+/* Licensed under Apache-2.0 2024. */
+package com.example.jdbc.service;
+
+import dagger.Module;
+
+@Module
+public interface ServiceModule {
+
+  JdbcService jdbcService();
+}
---
+++ jdbc-parent/jdbc-app/src/main/java/com/example/jdbc/verticle/DefaultVerticle.java
@@ -0,0 +1,43 @@
+/* Licensed under Apache-2.0 2024. */
+package com.example.jdbc.verticle;
+
+import com.example.commons.config.Config;
+import com.example.jdbc.ioc.DaggerProvider;
+import com.example.jdbc.ioc.Provider;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.json.JsonObject;
+import java.util.Objects;
+
+public class DefaultVerticle extends AbstractVerticle {
+
+  private Provider dagger;
+  private JdbcVerticle jdbcVerticle;
+
+  private void init() {
+    JsonObject cfg = config();
+    Config config = Config.fromJson(cfg);
+
+    Objects.requireNonNull(vertx);
+    Objects.requireNonNull(config);
+    Objects.requireNonNull(config.httpConfig());
+    Objects.requireNonNull(config.postgresConfig());
+
+    this.dagger = DaggerProvider.builder().vertx(vertx).config(config).build();
+
+    this.dagger.init();
+  }
+
+  @Override
+  public void start(Promise<Void> startPromise) {
+    init();
+    jdbcVerticle = dagger.jdbcVerticle();
+    jdbcVerticle.init(vertx, context);
+    jdbcVerticle.start(startPromise);
+  }
+
+  @Override
+  public void stop(Promise<Void> stopPromise) {
+    jdbcVerticle.stop(stopPromise);
+  }
+}
