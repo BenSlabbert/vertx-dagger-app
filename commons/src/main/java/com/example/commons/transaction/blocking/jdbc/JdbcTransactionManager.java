a8a3a9d28db86d9a31f91fcf70bf80a99cd88fe7
benjamin.slabbert <benjamin.slabbert@gmail.com>
2024-05-05 15:00:55 +0200

    adding jdbc utils

--- commons/src/main/java/com/example/commons/transaction/blocking/jdbc/JdbcTransactionManager.java
+++ commons/src/main/java/com/example/commons/transaction/blocking/jdbc/JdbcTransactionManager.java
@@ -1,17 +1,30 @@
+/* Licensed under Apache-2.0 2024. */
 package com.example.commons.transaction.blocking.jdbc;
 
 import java.sql.Connection;
+import javax.inject.Inject;
+import javax.inject.Singleton;
 import javax.sql.DataSource;
 import org.apache.commons.dbutils.DbUtils;
 
+@Singleton
 public class JdbcTransactionManager {
 
   private static final ThreadLocal<Connection> connThreadLocal = new ThreadLocal<>();
 
   private final DataSource dataSource;
 
+  @Inject
   JdbcTransactionManager(DataSource dataSource) {
     this.dataSource = dataSource;
+  }
+
+  public Connection getConnection() {
+    Connection connection = connThreadLocal.get();
+    if (null == connection) {
+      throw new IllegalStateException("cannot get connection: no transaction in progress");
+    }
+    return connection;
   }
 
   public void begin() {
@@ -30,7 +43,7 @@ public class JdbcTransactionManager {
   public void commit() {
     Connection connection = connThreadLocal.get();
     if (null == connection) {
-      throw new IllegalStateException("no transaction in progress");
+      throw new IllegalStateException("cannot commit: no transaction in progress");
     }
 
     try {
@@ -45,7 +58,7 @@ public class JdbcTransactionManager {
   public void rollback() {
     Connection connection = connThreadLocal.get();
     if (null == connection) {
-      throw new IllegalStateException("no transaction in progress");
+      throw new IllegalStateException("cannot rollback: no transaction in progress");
     }
 
     try {
