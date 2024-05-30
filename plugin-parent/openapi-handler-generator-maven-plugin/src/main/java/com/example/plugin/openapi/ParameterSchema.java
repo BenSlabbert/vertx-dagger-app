cb3c01b4c9efa57613f50ce989c9a82a10b094ec
benjamin.slabbert <benjamin.slabbert@gmail.com>
2024-05-30 15:02:18 +0200

    adding initial helm template]remvinh maven plugin

--- plugin-parent/openapi-handler-generator-maven-plugin/src/main/java/com/example/plugin/openapi/ParameterSchema.java
+++
@@ -1,7 +0,0 @@
-/* Licensed under Apache-2.0 2024. */
-package com.example.plugin.openapi;
-
-import com.example.plugin.openapi.type.In;
-import com.example.plugin.openapi.type.ParamType;
-
-record ParameterSchema(String name, ParamType type, boolean required, In in) {}
