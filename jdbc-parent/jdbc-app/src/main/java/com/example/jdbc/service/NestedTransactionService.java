b6b6b4ea348f62c29fa625c8b5f431bb7a140eb1
benjamin.slabbert <benjamin.slabbert@gmail.com>
2025-01-12 13:29:04 +0100

    fix build

--- jdbc-parent/jdbc-app/src/main/java/com/example/jdbc/service/NestedTransactionService.java
+++ jdbc-parent/jdbc-app/src/main/java/com/example/jdbc/service/NestedTransactionService.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Singleton
-@Transactional
 public class NestedTransactionService {
 
   private static final Logger log = LoggerFactory.getLogger(NestedTransactionService.class);
