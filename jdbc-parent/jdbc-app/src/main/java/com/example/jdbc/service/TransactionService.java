65e2c1d295ad2b53dad91e4fb2ef2fa8d4692482
benjamin.slabbert <benjamin.slabbert@gmail.com>
2025-01-12 13:29:04 +0100

    fix build

--- jdbc-parent/jdbc-app/src/main/java/com/example/jdbc/service/TransactionService.java
+++ jdbc-parent/jdbc-app/src/main/java/com/example/jdbc/service/TransactionService.java
@@ -14,7 +14,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Singleton
-@Transactional
 public class TransactionService {
 
   private static final Logger log = LoggerFactory.getLogger(TransactionService.class);
